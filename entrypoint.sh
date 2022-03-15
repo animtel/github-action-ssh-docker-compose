e664b11964a352c5242d53e8c96acf7120daae8d
Kovalenko Daniel <animtel@gmail.com>
2022-03-15 17:54:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ cleanup() {
   log "Killing ssh agent."
   ssh-agent -k
   log "Removing workspace archive."
-  rm -f /tmp/$DEPLOY_DIR_NAME.tar.bz2
+  sudo rm -f /tmp/$DEPLOY_DIR_NAME.tar.bz2
 }
 trap cleanup EXIT
 
@@ -20,7 +20,7 @@ tar cjvf /tmp/$DEPLOY_DIR_NAME.tar.bz2 --exclude .git .
 log "Launching ssh agent."
 eval `ssh-agent -s`
 
-remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; log 'Removing workspace...' ; rm -rf \"\$HOME/$DEPLOY_DIR_NAME\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/$DEPLOY_DIR_NAME\" ; log 'Unpacking workspace...' ; tar -C \"\$HOME/$DEPLOY_DIR_NAME\" -xjv ; log 'Launching docker-compose...' ; cd \"\$HOME/$DEPLOY_DIR_NAME\" ; docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build"
+remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; log 'Removing workspace...' ; sudo rm -rf \"\$HOME/$DEPLOY_DIR_NAME\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/$DEPLOY_DIR_NAME\" ; log 'Unpacking workspace...' ; tar -C \"\$HOME/$DEPLOY_DIR_NAME\" -xjv ; log 'Launching docker-compose...' ; cd \"\$HOME/$DEPLOY_DIR_NAME\" ; docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build"
 
 ssh-add <(echo "$SSH_PRIVATE_KEY")
 
