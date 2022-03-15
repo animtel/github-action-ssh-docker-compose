99327fd58bec284d1c2e71020ddfcf5e5895e90c
Kovalenko Daniel <animtel@gmail.com>
2022-03-15 17:37:15 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,20 +10,17 @@ cleanup() {
   log "Killing ssh agent."
   ssh-agent -k
   log "Removing workspace archive."
-  rm -f /tmp/workspace.tar.bz2
+  rm -f /tmp/$DEPLOY_DIR_NAME.tar.bz2
 }
 trap cleanup EXIT
 
 log "Packing workspace into archive to transfer onto remote machine."
-tar cjvf /tmp/workspace.tar.bz2 --exclude .git .
+tar cjvf /tmp/$DEPLOY_DIR_NAME.tar.bz2 --exclude .git .
 
 log "Launching ssh agent."
 eval `ssh-agent -s`
 
-remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; log 'Removing workspace...' ; rm -rf \"\$HOME/workspace\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/workspace\" ; log 'Unpacking workspace...' ; tar -C \"\$HOME/workspace\" -xjv ; log 'Launching docker-compose...' ; cd \"\$HOME/workspace\" ; docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build"
-if $USE_DOCKER_STACK ; then
-  remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/workspace\" ; } ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/workspace/$DOCKER_COMPOSE_PREFIX\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/workspace/$DOCKER_COMPOSE_PREFIX\" -xjv ; log 'Launching docker stack deploy...' ; cd \"\$HOME/workspace/$DOCKER_COMPOSE_PREFIX\" ; docker stack deploy -c \"$DOCKER_COMPOSE_FILENAME\" --prune \"$DOCKER_COMPOSE_PREFIX\""
-fi
+remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; log 'Removing workspace...' ; rm -rf \"\$HOME/$DEPLOY_DIR_NAME\" ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/$DEPLOY_DIR_NAME\" ; log 'Unpacking workspace...' ; tar -C \"\$HOME/$DEPLOY_DIR_NAME\" -xjv ; log 'Launching docker-compose...' ; cd \"\$HOME/$DEPLOY_DIR_NAME\" ; docker-compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up -d --remove-orphans --build"
 
 ssh-add <(echo "$SSH_PRIVATE_KEY")
 
@@ -31,4 +28,4 @@ echo ">> [local] Connecting to remote host."
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
   "$SSH_USER@$SSH_HOST" -p "$SSH_PORT" \
   "$remote_command" \
-  < /tmp/workspace.tar.bz2
+  < /tmp/$DEPLOY_DIR_NAME.tar.bz2
